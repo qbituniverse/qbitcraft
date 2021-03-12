40b44b29cde8fb9f474a444269f93eed7ea82e54
qbituniverse <48298870+qbituniverse@users.noreply.github.com>
2021-03-12 11:07:32 +0000

    Initial tag test load to create Incremental & Release. (#8)
    Co-authored-by: Marcin Narloch <marcin.narloch@qbituniverse.com>

--- README.md
+++ README.md
@@ -1,15 +1,22 @@
 # Qbitcraft
-Qbitcraft, a modded version of minicraft+ that adds many more features to the original version.
 
-> This repo is forked from version [2.0.6](https://github.com/chrisj42/minicraft-plus-revived/releases/tag/v2.0.6) of this GitHub repo [minicraft-plus-revived](https://github.com/chrisj42/minicraft-plus-revived).
+Qbitcraft goal is to expand Minicraft+ and use it as a Java learning sample for aspiring coders.
+
+Modded by David.b and +Dillyg10+ until 1.8, then taken over by Chris J until 2.0.4, then by afyber, and then by chrisj42.
+
+Minicraft was originally made by Markus Perrson for ludum dare 22 competition.
+
+> This repo is forked from version [2.0.6 of chrisj42/minicraft-plus-revived](https://github.com/chrisj42/minicraft-plus-revived/releases/tag/v2.0.6).
 
 > Version going forward with QbitUniverse will be **1.0.0**.
 
-## Release 1.0.0
+# Changelog
 
-- Refactored
+## Version 1.0.0
 
-## Features Backlog
+- Initial refactoring
+
+# Backlog
 
 - Banana:
     - Fruit which gives 3 seconds super power to break anything with just one hit
--- src/main/java/qbitcraft/screen/BookData.java
+++ src/main/java/qbitcraft/screen/BookData.java
@@ -6,9 +6,15 @@ import qbitcraft.saveload.Load;
 
 public class BookData {
 	
-	public static final String about = "Modded by David.b and +Dillyg10+ until 1.8, then taken over by Chris J until 2.0.4, and then by afyber. Our goal is to expand Minicraft+ to be more fun and continuous.\nMinicraft was originally made by Markus Perrson for ludum dare 22 competition.";
+	public static final String about = "Modded by David.b and +Dillyg10+ until 1.8, then taken over by " +
+			"Chris J until 2.0.4, then by afyber, and then by chrisj42. " +
+			"Qbitcraft goal is to expand Minicraft+ and use it as a Java learning sample for aspiring coders.\n" +
+			"Minicraft was originally made by Markus Perrson for ludum dare 22 competition.";
 	
-	public static final String instructions = "With the default controls...\n\nMove your character with arrow keys or WSAD. Press C to attack and X to open the inventory, and to use items. Pickup furniture and torches with V. Select an item in the inventory to equip it.\n\nThe Goal: Defeat the air wizard!";
+	public static final String instructions = "With the default controls..." +
+			"\n\nMove your character with arrow keys or WSAD. Press C to attack and X to open the inventory, " +
+			"and to use items. Pickup furniture and torches with V. Select an item in the inventory to equip it.\n\n" +
+			"The Goal: Defeat the air wizard!";
 	
 	public static final String antVenomBook = loadBook("antidous");
 	public static final String storylineGuide = loadBook("story_guide");
